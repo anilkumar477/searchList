0a9b06dd1d949af33838befea4b611be3729a2aa
anil kumar nodagala <anilnodagala.123@gmail.com>
2024-07-10 09:30:05 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import { foods, filterList } from './datas';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function FoodList(){
+  const[query,setQuery] = useState('')
+  const results = filterList(foods,query)
+
+  function handleChange(e){
+    setQuery(e.target.value)
+  }
+
+  return(
+    <>
+    <SearchBar 
+    query={query}
+    onChange={handleChange}
+    />
+    <hr></hr>
+    <List items={results}/>
+    </>
+  )
 }
 
-export default App;
+function SearchBar({query,onChange}){
+  return(
+    <>
+    <label>
+      Search : 
+      {' '}
+      <input
+      value={query}
+      onChange={onChange}
+      />
+      
+    </label>
+    </>
+
+
+  )
+}
+
+function List({items}){
+  return(
+    <table>
+      <tbody>
+        {items.map(food => (
+          <tr key={food.id}>
+            <td>{food.name}</td>
+            <td>{food.description}</td>
+
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
